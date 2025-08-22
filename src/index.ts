5b189cb60489f7b716e024619e9cb4075f6796b3
Miika Sipilä <miikavsipila@gmail.com>
2025-08-22 10:35:34 +0300

    feat: integrate dotenv for environment variable management and establish MongoDB connection
    - Added dotenv configuration to load environment variables from a .env file.
    - Refactored the server initialization to connect to MongoDB before starting the Express server.
    - Implemented error handling for missing DB_URL in the database connection utility.
    - Enhanced MongoDB connection error logging for better debugging.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoConnect from './utils/db';
 
+const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+
+(async () => {
+  try {
+    await mongoConnect();
+    app.listen(port, () => {
+      console.log(`Listening: http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log('Server error', (error as Error).message);
+  }
+})();
+
+// app.listen(port, () => {
+//   console.log(`Listening: http://localhost:${port}`);
+// });
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,21 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import CustomError from '../classes/CustomError';
 dotenv.config();
 
 const mongoConnect = async () => {
   // Connect to MongoDB
+  if (!process.env.DB_URL) {
+    throw new CustomError('DB_URL is not defined', 500);
+  }
+
+  try {
+    const connection = await mongoose.connect(process.env.DB_URL);
+    console.log('MongoDB connected');
+    return connection;
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
 };
 
 export default mongoConnect;
